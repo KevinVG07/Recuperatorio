40450580091c4c0550244983d3857e5294939e00
KevinVG07 <kevinvg08@gmail.com>
2020-04-29 01:07:50 -0300

    Traaaavis

--- src/practicaPB2/ClaseTravis.java
+++ src/practicaPB2/ClaseTravis.java
@@ -1,5 +1,11 @@
 package practicaPB2;
 
 public class ClaseTravis {
-
+	private Integer show;
+	private String watafa;
+	
+	public ClaseTravis(){
+			this.show = 10;
+			this.watafa = "Amazing";
+	}
 }
